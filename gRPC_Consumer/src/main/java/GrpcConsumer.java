ce2fe3eb4d0a20191840f791b1c63f6cab4db04c
Sumonta Saha Mridul <61287791+Sumonta056@users.noreply.github.com>
2023-10-09 20:42:55 +0600

    [Base] gRPC Producer-Consumer Base Creation

---
+++ gRPC_Consumer/src/main/java/GrpcConsumer.java
@@ -0,0 +1,86 @@
+import com.demo.grpc.User;
+import com.demo.grpc.userGrpc;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
+import java.util.Scanner;
+import java.util.logging.Logger;
+
+public class GrpcConsumer {
+
+    private static final Logger logger = Logger.getLogger(GrpcConsumer.class.getName());
+
+    public static void main(String[] args) {
+        ManagedChannel channel = ManagedChannelBuilder
+                .forAddress("localhost", 8082)
+                .usePlaintext()
+                .build();
+
+        userGrpc.userBlockingStub userStub = userGrpc.newBlockingStub(channel);
+
+        // Create a Scanner to capture user input
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompt the user to choose between login and registration
+        System.out.println("Choose an option:");
+        System.out.println("1. Login");
+        System.out.println("2. Register");
+        System.out.print("Enter your choice (1 or 2): ");
+        int choice = scanner.nextInt();
+        scanner.nextLine(); // Consume newline
+
+        if (choice == 1) {
+            // User chose login
+            System.out.print("Enter your username: ");
+            String username = scanner.nextLine();
+
+            System.out.print("Enter your password: ");
+            String password = scanner.nextLine();
+
+            // Build the gRPC request with user input
+            User.LoginReq request = User.LoginReq.newBuilder()
+                    .setUsername(username)
+                    .setPassword(password)
+                    .build();
+
+            // Send the login request to the server
+            User.APIRes apiRes = userStub.login(request);
+            logger.info(apiRes.getResCode() + "\n" + apiRes.getMessage());
+        } else if (choice == 2) {
+            // User chose registration
+            System.out.print("Enter your username: ");
+            String username = scanner.nextLine();
+
+            System.out.print("Enter your password: ");
+            String password = scanner.nextLine();
+
+            System.out.print("Enter your name: ");
+            String name = scanner.nextLine();
+
+            System.out.print("Enter your email: ");
+            String email = scanner.nextLine();
+
+            System.out.print("Enter your bio: ");
+            String bio = scanner.nextLine();
+
+            // Build the gRPC request for registration
+            User.RegisterReq registerReq = User.RegisterReq.newBuilder()
+                    .setUsername(username)
+                    .setPassword(password)
+                    .setName(name)
+                    .setEmail(email)
+                    .setBio(bio)
+                    .build();
+
+            // Send the registration request to the server
+            User.APIRes apiRes = userStub.register(registerReq);
+            logger.info(apiRes.getResCode() + "\n" + apiRes.getMessage());
+        } else {
+            System.out.println("Invalid choice.");
+        }
+
+        // Close the scanner and channel
+        scanner.close();
+        channel.shutdown();
+    }
+}
---
+++ gRPC_Producer/src/main/java/GrpcProducer.java
@@ -0,0 +1,23 @@
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import services.UserService;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
+
+public class GrpcProducer {
+
+    private static final Logger logger = Logger.getLogger(GrpcProducer.class.getName());
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Server server = ServerBuilder
+                .forPort(8082)
+                .addService(new UserService())
+                .build();
+        server.start();
+
+        logger.info("Server started at port : " + server.getPort());
+          server.awaitTermination(60, TimeUnit.SECONDS);
+    }
+}
